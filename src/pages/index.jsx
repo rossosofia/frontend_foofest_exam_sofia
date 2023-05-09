347e511c186eddc089db25af4236103da48a51cd
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-09 12:34:07 +0200

    Initializing the project

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-blue-500 py-4">
+        FooFest Extravaganza
+      </h1>
+    </>
+  );
+}

b26fc75058eb003bb623e21a175fa730c173c356
sofia amoroso <sofi79a0@stud.kea.dk>
2023-05-11 13:23:09 +0200

    steps components

---
+++ src/components/Steps.jsx
@@ -0,0 +1,58 @@
+{
+  /*
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+*/
+}
+
+export default function Steps() {
+  return (
+    <div>
+      <h2 className="sr-only">Steps</h2>
+
+      <div className="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100">
+        <ol className="relative z-10 flex justify-between text-sm font-medium text-gray-500">
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+              1
+            </span>
+
+            <span className="hidden sm:block"> Tickets </span>
+          </li>
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+              2
+            </span>
+
+            <span className="hidden sm:block"> Accomodations </span>
+          </li>
+
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+              3
+            </span>
+
+            <span className="hidden sm:block"> Guests info </span>
+          </li>
+
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+              4
+            </span>
+
+            <span className="hidden sm:block"> Payment </span>
+          </li>
+
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+              5
+            </span>
+
+            <span className="hidden sm:block"> Thanks </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import Steps from "@/components/Steps";
+
 export default function Home() {
   return (
     <>

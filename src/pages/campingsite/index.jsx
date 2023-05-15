7e91a249cf82a3fe08d806fd87aa61446bfcedf7
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-15 11:32:58 +0200

    Created the correct cards for capmingsite

--- src/pages/campingsite/index.jsx
+++ src/pages/campingsite/index.jsx
@@ -1,33 +1,33 @@
 import SelectCard from "@/components/SelectCard";
 
-export default function Campingsite() {
+export default function Campingsite({ data }) {
   return (
     <>
       <h1 className="text-4xl font-bold text-center text-blue-500 py-4">
         Choose your camping site
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-        <SelectCard
-          title="Viking Village"
-          description="A Nordic-themed camping area, transporting guests to the days of the Vikings with longhouses and rune-carved totems."
-        />
-        <SelectCard
-          title="Auora Acres"
-          description="A tranquil camping area featuring mesmerizing light installations, creating a magical ambiance reminiscent of the Northern Lights."
-        />
-        <SelectCard
-          title="Fjord Forest"
-          description="A serene, forested area where campers can immerse themselves in nature, inspired by the beauty of Scandinavian landscapes."
-        />
-        <SelectCard
-          title="Mytich Meadows"
-          description="A whimsical camping area, filled with colorful tents and playful sculptures inspired by Scandinavian folklore and mythology."
-        />
-        <SelectCard
-          title="Midnight Sun Santuary"
-          description="A premium camping area offering upgraded facilities, wellness activities, and a soothing ambiance, reminiscent of the Scandinavian summer nights."
-        />
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-4">
+        {data.map((areas) => (
+          <SelectCard key={data.area} {...areas} />
+        ))}
+      </div>
+
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    "http://brazen-fortune-fight.glitch.me//available-spots"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }

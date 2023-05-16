0e6b88037f8acea1a7f34dd705f62c11d5fc640d
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-16 13:36:44 +0200

    Formatting

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -1,27 +1,25 @@
 import { useRouter } from "next/router";
 
- 
-//we do that so we dont download data we dont need 
+//we do that so we dont download data we dont need
 export default function Anchor({ children, href, className, disabled }) {
-    const router = useRouter();
-  
-    function handleClick(e) {
-      e.preventDefault();
-      if (!disabled) {
-        router.push(href);
-      }
+  const router = useRouter();
+
+  function handleClick(e) {
+    e.preventDefault();
+    if (!disabled) {
+      router.push(href);
     }
-  
-    return (
-      <a
-        className={`inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ${
-          disabled ? 'opacity-50 cursor-not-allowed' : ''
-        } ${className}`}
-        href={href}
-        onClick={handleClick}
-      >
-        {children}
-      </a>
-    );
   }
-  
+
+  return (
+    <a
+      className={`inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ${
+        disabled ? "opacity-50 cursor-not-allowed" : ""
+      } ${className}`}
+      href={href}
+      onClick={handleClick}
+    >
+      {children}
+    </a>
+  );
+}

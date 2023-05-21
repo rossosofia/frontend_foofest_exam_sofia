b4956e207f85bd59a5049a230a284050d9d31171
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-21 09:29:50 +0200

    Made a page for all bands, and updated style

---
+++ src/pages/all-bands/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import Anchor from "@/components/Anchor";
+
+export default function AllBands() {
+  const [bands, setBands] = useState(null);
+
+  useEffect(() => {
+    fetch("https://brazen-fortune-fight.glitch.me/bands")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(setBands)
+      .catch(console.error);
+  }, []);
+
+  if (bands === null) {
+    return "Loading...";
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-8">All Bands</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {bands.map((band) => (
+          <div key={band.name} className="bg-white rounded shadow p-4">
+            <h2 className="text-xl font-bold mb-2">{band.name}</h2>
+            <p className="text-gray-600">Genre: {band.genre}</p>
+            <Anchor href={`/bands/${band.slug}`} className="text-blue-500 mt-2">
+              View Details
+            </Anchor>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -33,33 +33,36 @@ export default function Schedule() {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-4 p-4">
-      {Object.entries(schedule).map(([stage, days]) => (
-        <div key={stage} className="bg-blue-200 p-4 rounded">
-          <h2 className="text-2xl mb-4">{stage}</h2>
-          {Object.entries(days).map(([day, events], index) => (
-            <div key={day}>
-              {index > 0 && <hr className="my-4" />} {/* Add a separator */}
-              <h3 className="text-xl mb-2">{formatDay(day)}</h3>{" "}
-              {/* Format the day name */}
-              <ul className="list-disc list-inside mb-4">
-                {events.map((event, index) => (
-                  <li key={index} className="mb-1">
-                    {event.start} - {event.end} :{" "}
-                    {event.act === "break" ? (
-                      event.act
-                    ) : (
-                      <Anchor href={`/bands/${getSlug(event.act)}`}>
-                        {event.act}
-                      </Anchor>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      ))}
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-8">Schedule</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {Object.entries(schedule).map(([stage, days]) => (
+          <div key={stage} className="bg-white rounded shadow p-4">
+            <h2 className="text-2xl font-bold mb-4">{stage}</h2>
+            {Object.entries(days).map(([day, events], index) => (
+              <div key={day}>
+                {index > 0 && <hr className="my-4" />} {/* Add a separator */}
+                <h3 className="text-xl mb-2">{formatDay(day)}</h3>{" "}
+                {/* Format the day name */}
+                <ul className="list-disc list-inside mb-4">
+                  {events.map((event, index) => (
+                    <li key={index} className="mb-1">
+                      {event.start} - {event.end} :{" "}
+                      {event.act === "break" ? (
+                        event.act
+                      ) : (
+                        <Anchor href={`/bands/${getSlug(event.act)}`}>
+                          {event.act}
+                        </Anchor>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

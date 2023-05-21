a737922e55c6ee288e2a33e75c73a1db56576e46
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-21 11:05:33 +0200

    Made confetti work on thanks page

--- src/pages/thanks/index.jsx
+++ src/pages/thanks/index.jsx
@@ -1,10 +1,43 @@
-import FlowLayout from "@/components/FlowLayout";
+import React, { useEffect } from "react";
+import confetti from "canvas-confetti";
 
 export default function Thanks() {
-  return (
-    <FlowLayout>
-      <h1>Step 5</h1>
-      <p>This is the content for Step 5.</p>
-    </FlowLayout>
-  );
+  useEffect(() => {
+    let duration = 7 * 1000;
+    let animationEnd = Date.now() + duration;
+    let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    let interval = setInterval(function () {
+      let timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      let particleCount = 50 * (timeLeft / duration);
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+      );
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+      );
+    }, 250);
+
+    // Make sure to clear the interval when the component is unmounted
+    return () => {
+      clearInterval(interval);
+    };
+  }, []); // The empty array ensures this runs on mount and unmount only
+
+  return <></>;
 }

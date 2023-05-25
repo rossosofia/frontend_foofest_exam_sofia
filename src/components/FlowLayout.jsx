0680ec6ae59e512115cd49938968933cdd2e1672
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-25 11:07:30 +0200

    I fixed the stepper res issues + added card thanks

--- src/components/FlowLayout.jsx
+++ src/components/FlowLayout.jsx
@@ -1,7 +1,5 @@
-import { Stepper, Step, StepLabel } from "@mui/material";
 import { useRouter } from "next/router";
 import Basket from "./Basket";
-import Anchor from "./Anchor";
 
 export default function FlowLayout({ children }) {
   const router = useRouter();
@@ -16,15 +14,29 @@ export default function FlowLayout({ children }) {
   const currentStep = steps.findIndex((step) => router.pathname === step.path);
 
   return (
-    <div className="m-10 p-5">
-      <Stepper activeStep={currentStep} alternativeLabel>
-        {steps.map((step) => (
-          <Step key={step.label}>
-            <StepLabel>{step.label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-
+    <div className="m-5 p-5">
+      <div>
+        <h2 className="sr-only">Steps</h2>
+        <div className="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100">
+          <ol className="relative z-10 flex justify-between text-sm font-medium text-gray-500">
+            {steps.map((step, index) => (
+              <li
+                key={step.label}
+                className="flex items-center gap-2 bg-white p-2"
+              >
+                <span
+                  className={`h-6 w-6 rounded-full text-center text-[10px]/6 font-bold ${
+                    currentStep >= index ? "bg-black text-white" : "bg-gray-100"
+                  }`}
+                >
+                  {index + 1}
+                </span>
+                <span className="hidden sm:block"> {step.label} </span>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
       <div className="mt-5">{children}</div>
       <Basket />
     </div>
--- src/pages/thanks/index.jsx
+++ src/pages/thanks/index.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useContext } from "react";
 import confetti from "canvas-confetti";
 import { useRouter } from "next/router";
-import { DispatchContext } from "@/context/storeContext";
+import { DispatchContext, StoreContext } from "@/context/storeContext";
+import Barcode from "react-barcode";
 
 export default function Thanks() {
   const router = useRouter();
   const dispatch = useContext(DispatchContext);
+  const state = useContext(StoreContext);
 
   useEffect(() => {
     let isMounted = true;
@@ -53,5 +55,31 @@ export default function Thanks() {
     };
   }, [dispatch, router]);
 
-  return <></>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4">
+        <div className="md:flex">
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-black font-semibold">
+              Festival Ticket
+            </div>
+            <h2 className="block mt-1 text-lg leading-tight font-medium text-black">
+              Congratulations, you are going to the Foofest!
+            </h2>
+            <p className="mt-2 text-gray-500">Here are your ticket details:</p>
+            {state.ticketBasket.map((item, index) => (
+              <div key={index}>
+                <h3>Ticket type: {item.name}</h3>
+                <p>Price: {item.price}</p>
+                <p>Amount: {item.amount}</p>
+                <p>Total: {item.price * item.amount}</p>
+                <Barcode value="1234567890" />
+              </div>
+            ))}
+            {/* Rest of your code */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

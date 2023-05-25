5a7772954fbeef685ab096b632edafc5ac722f13
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-25 15:20:20 +0200

    Clearing basket on campingsite, which was deleted?

--- src/pages/campingsite/index.jsx
+++ src/pages/campingsite/index.jsx
@@ -1,7 +1,15 @@
 import CampingCard from "@/components/CampingCard";
 import Layout from "@/components/Layout";
+import { useContext, useEffect } from "react";
+import { DispatchContext } from "@/context/storeContext";
 
 export default function Campingsite({ data }) {
+  const dispatch = useContext(DispatchContext);
+
+  useEffect(() => {
+    dispatch({ action: "EMPTY_BASKET" });
+  }, [dispatch]);
+
   return (
     <Layout>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-4 p-4">

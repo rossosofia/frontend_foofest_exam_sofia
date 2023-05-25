e1c9197c31dfc6ebe12a119037c1075c6d58adef
sofia amoroso <sofi79a0@stud.kea.dk>
2023-05-25 13:19:57 +0200

    nav bar and menu style + margins

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -22,7 +22,7 @@ export default function Anchor({
 
   return (
     <a
-      className={`inline-block bg-white hover:bg-gray-100 border-2 border-black transition-transform duration-500 ease-in-out transform hover:-translate-x-1 hover:-translate-y-1 text-black font-bold py-2 px-4 rounded ${
+      className={`inline-block bg-white hover:bg-gray-100 border-2 border-black transition-transform duration-500 ease-in-out transform hover:-translate-x-1 hover:-translate-y-1 text-black font-bold py-4 px-6 rounded ${
         disabled ? "opacity-50 cursor-not-allowed" : ""
       } ${className}`}
       href={href}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,24 +7,22 @@ export default function Home() {
     <Layout>
       <Anchor
         href="/campingsite/"
-        className="fixed bottom-5 right-5 text-2xl z-50"
+        className="fixed bottom-5 right-5 text-4xl z-50"
       >
-        BUY TICKETS <span className="ml-2">→</span>
+        <p className="font-normal text-sm">FooFest Extravaganza 2023</p>
+        BUY TICKETS <span className="ml-2 ">→</span>
       </Anchor>
-      <section
-        className="mx-auto h-[70vh] flex flex-col justify-center items-center 
-    bg-gradient-to-r from-custom-purple via-custom-beige to-custom-red"
-      >
-        <h1 className="text-4xl font-bold text-center text-black-500 py-4">
-          FOOFEST EXTRAVAGANZA
+      <section className="h-screen flex flex-col justify-center items-center px-10 ">
+        <h1 className="text-8xl font-bold text-center py-4">
+          FOOFEST <br></br> EXTRAVAGANZA
         </h1>
-        <p className="font-bold text-center text-2xl pb-2">1 - 7 July 2023</p>
-        <p className="text-center text-2xl">Fantasy Island</p>
+        <p className="font-bold text-center text-4xl pb-2">1 - 7 July 2023</p>
+        <p className="text-center text-4xl">Fantasy Island</p>
       </section>
 
-      <section className="pl-5 pr-5 border-t-2 border-black ">
+      <section className="px-10">
         <div className="flex justify-between items-center my-4">
-          <h2 className="text-2xl font-bold">Featured Artists</h2>
+          <h2 className="text-4xl text-white font-bold">FEATURED ARTISTS</h2>
           <Anchor href="/schedule/">
             FULL LINE-UP<span className="ml-2">→</span>
           </Anchor>
@@ -35,10 +33,15 @@ export default function Home() {
           <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
           <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
           <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
+          <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
+          <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
+          <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
+          <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
+          <div className="flex-shrink-0 w-64 h-64 m-2 bg-gray-300"></div>
         </div>
       </section>
 
-      <section className="mx-auto h-[70vh] flex flex-col md:flex-row items-center justify-center my-4 p-5 border-t-2 border-black">
+      <section className="mx-auto h-[70vh] flex flex-col md:flex-row items-center justify-center my-4 px-10 border-t-2 border-black">
         <div className="mb-4 md:mb-0 md:mr-4 md:flex-1">
           <h2 className="text-2xl font-bold">Join the Community</h2>
           <p>

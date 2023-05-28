9ae25c0d5954e011c30947b1e82cdec854dcaa77
sofia amoroso <sofi79a0@stud.kea.dk>
2023-05-28 16:49:43 +0200

    camping site style and copy

--- src/components/CampingCard.jsx
+++ src/components/CampingCard.jsx
@@ -4,6 +4,34 @@ import { useContext } from "react";
 import { DispatchContext } from "@/context/storeContext";
 import { useRouter } from "next/router";
 
+const campingAreaDescriptions = [
+  {
+    area: "Svartheim",
+    description:
+      "A Nordic-themed camping area, transporting guests to the days of the Vikings with longhouses and rune-carved totems.",
+  },
+  {
+    area: "Nilfheim",
+    description:
+      "A tranquil camping area featuring mesmerizing light installations, creating a magical ambiance reminiscent of the Northern Lights.",
+  },
+  {
+    area: "Helheim",
+    description:
+      "A serene, forested area where campers can immerse themselves in nature, inspired by the beauty of Scandinavian landscapes.",
+  },
+  {
+    area: "Muspelheim",
+    description:
+      "A whimsical camping area, filled with colorful tents and playful sculptures inspired by Scandinavian folklore and mythology.",
+  },
+  {
+    area: "Alfheim",
+    description:
+      "A premium camping area offering upgraded facilities, wellness activities, and a soothing ambiance, reminiscent of the Scandinavian summer nights.",
+  },
+];
+
 const CampingCard = (props) => {
   const dispatch = useContext(DispatchContext);
   const router = useRouter();
@@ -23,14 +51,17 @@ const CampingCard = (props) => {
   }
 
   const isAvailable = props.available > 0;
+  const description = campingAreaDescriptions.find(
+    (item) => item.area === props.area
+  )?.description;
 
   return (
     <>
-      <div className="group relative block h-screen max-h-[80vh]">
+      <div className="group relative block max-h-[80vh]">
         <span className="absolute inset-0 border-2 border-dashed border-blue"></span>
 
         <div className="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
-          <div className=" transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+          <div className="transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
             <h2 className="m-2 text-xl font-medium sm:text-2xl">
               {props.area}
             </h2>
@@ -42,14 +73,10 @@ const CampingCard = (props) => {
             </h3>
 
             <p className="mt-4 mb-4 text-sm sm:text-base">
-              {props.available} / {props.spots}
+              Spots available {props.available} / {props.spots}
             </p>
 
-            <p className="mt-4 mb-4 text-sm sm:text-base">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Doloremque exercitationem eveniet a dolorem? Quia, aspernatur
-              culpa mollitia dicta tempore inventore?
-            </p>
+            <p className="mt-4 mb-4 text-sm sm:text-base">{description}</p>
 
             <Anchor
               href="/tickets/"
--- src/pages/campingsite/index.jsx
+++ src/pages/campingsite/index.jsx
@@ -9,14 +9,25 @@ export default function Campingsite({ data }) {
   useEffect(() => {
     dispatch({ action: "EMPTY_BASKET" });
   }, [dispatch]);
-
+  // pt-10 px-10 mt-[5rem]
   return (
     <Layout>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-4 p-4">
-        {data.map((areas) => (
-          <CampingCard key={areas.area} {...areas} />
-        ))}
-      </div>
+      <section className="flex flex-col justify-between px-10 h-screen">
+        <div>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-white py-4 pt-10 mt-[5rem]">
+            Discover our camping areas
+          </h1>
+          <h2 className="text-2xl md:text-2xl text-white font-light pb-10 w-full lg:w-1/2">
+            FooFest Extravaganza offers five distinct camping areas, each with
+            its own unique atmosphere and amenities.
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-4 py-5 items-end">
+          {data.map((areas) => (
+            <CampingCard key={areas.area} {...areas} />
+          ))}
+        </div>
+      </section>
     </Layout>
   );
 }
